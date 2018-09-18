7cdc62376c1d5752f8439796aeb987aa308ffade
andreazoDevman <andreazo2012@gmail.com>
2018-09-17 21:13:11 -0300

    testando jenkis

--- src/main/java/iseries/controller/HomeController.java
+++ src/main/java/iseries/controller/HomeController.java
@@ -29,7 +29,7 @@ public class HomeController {
 		Collections.shuffle(series);
 		model.addAttribute("serie", series.get(0));
 		*/
-		return "index";
+		return "Funcionando Jenkis e github";
 	}
 	
 	// Página Inicial
