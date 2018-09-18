fcd507e89a09e0a0b656d4d129f78a1509044199
andreazoDevman <andreazo2012@gmail.com>
2018-09-17 21:52:10 -0300

    n

--- src/main/java/iseries/controller/HomeController.java
+++ src/main/java/iseries/controller/HomeController.java
@@ -29,7 +29,7 @@ public class HomeController {
 		Collections.shuffle(series);
 		model.addAttribute("serie", series.get(0));
 		*/
-		return " Jenkis e github";
+		return " Change Jenkis e github";
 	}
 	
 	// Página Inicial
