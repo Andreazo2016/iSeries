a5d1b125e835dd34ebeebb69946040edbe8f8fe8
andreazoDevman <andreazo2012@gmail.com>
2018-09-17 23:44:48 -0300

    t

--- src/main/java/iseries/controller/HomeController.java
+++ src/main/java/iseries/controller/HomeController.java
@@ -29,7 +29,7 @@ public class HomeController {
 		Collections.shuffle(series);
 		model.addAttribute("serie", series.get(0));
 		*/
-		return "Funcionando com with jenkins";
+		return "eh nois mano";
 	}
 	
 	// Página Inicial
