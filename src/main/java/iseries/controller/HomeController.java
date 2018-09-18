6e0d63066b18fca4410bb94aff300411b7b81cb2
andreazoDevman <andreazo2012@gmail.com>
2018-09-17 23:49:50 -0300

    t

--- src/main/java/iseries/controller/HomeController.java
+++ src/main/java/iseries/controller/HomeController.java
@@ -29,7 +29,7 @@ public class HomeController {
 		Collections.shuffle(series);
 		model.addAttribute("serie", series.get(0));
 		*/
-		return "eh nois mano : NOVO BUILD";
+		return "eh nois mano : NOVO BUILD xcfaa";
 	}
 	
 	// Página Inicial
