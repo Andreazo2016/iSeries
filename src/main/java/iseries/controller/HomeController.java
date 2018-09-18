5917ffd38ba2cd574dcccf9108310c20feedd489
andreazoDevman <andreazo2012@gmail.com>
2018-09-17 22:21:30 -0300

    n

--- src/main/java/iseries/controller/HomeController.java
+++ src/main/java/iseries/controller/HomeController.java
@@ -29,7 +29,7 @@ public class HomeController {
 		Collections.shuffle(series);
 		model.addAttribute("serie", series.get(0));
 		*/
-		return "Utimo with jenkins";
+		return "Funcionando com with jenkins";
 	}
 	
 	// Página Inicial
