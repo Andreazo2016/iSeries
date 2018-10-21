3215885578f427fcd524be50554f8d56fdcd2711
andreazoDevman <andreazo2012@gmail.com>
2018-10-21 14:22:59 -0300

    sucesso teste 2

--- src/main/java/iseries/controller/HomeController.java
+++ src/main/java/iseries/controller/HomeController.java
@@ -18,7 +18,7 @@ public class HomeController {
 
 	@RequestMapping("/")
 	@ResponseBody String  home(Model model){
-		return "TESTANDO A INTEGRAÇÃO";
+		return "Denvo TESTANDO";
 	}
 	
 	@RequestMapping("homeUsuario")
